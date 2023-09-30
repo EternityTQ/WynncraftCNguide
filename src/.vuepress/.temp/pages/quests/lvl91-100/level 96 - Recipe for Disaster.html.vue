cdbcb9a749eb5621b8583c24e1a218f55b7476cb
EternityTQ <1586049354@qq.com>
2023-09-30 21:46:51 +0800

    111

--- src/.vuepress/.temp/pages/quests/lvl91-100/level 96 - Recipe for Disaster.html.vue
+++ src/.vuepress/.temp/pages/quests/lvl91-100/level 96 - Recipe for Disaster.html.vue
@@ -4,19 +4,83 @@
 <p class="hint-container-title">注意</p>
 <p>此页面有待更新！</p>
 </div>
-<p>1跟在Ahmsord的Chef Hamsey对话X 949 Y 129 Z -4475</p>
-<p>2进去Chef Hamsey的厨房</p>
+<p>1跟在<strong>Ahmsord</strong>的<em>Chef Hamsey</em>对话<code v-pre>[949 129 -4475]</code></p>
+<p>2进去<em>Chef Hamsey</em>的厨房(身后)，在里面和跟<em>Chef Hamsey</em>谈话对话</p>
 <p>3跟Chef Hamsey谈话 然后跟其他三个npc谈话</p>
-<p>4选择一个厨师继续任务</p>
-<p>5到NPC给你的坐标的位置收集物品(背包里有本书记录了任务坐标,基本都是掉落物)</p>
-<p>6把材料给Chef Hamsey</p>
+<p>4再次跟Chef Hamsey谈话，选择一个厨师继续任务，然后找对应的NPC接活</p>
+<div class="hint-container tip">
+<p class="hint-container-title">提示</p>
+<p>NPC会给你一本食谱书，打开即可选择追踪材料</p>
+<p>一共需要收集三个材料</p>
+</div>
+<p>6.前往收集第一个材料</p>
+<Tabs id="34" :data='[{"id":"Brie"},{"id":"Kale"},{"id":"Frank"}]' tab-id="chef">
+<template #title0="{ value, isActive }">Brie</template>
+<template #title1="{ value, isActive }">Kale</template>
+<template #title2="{ value, isActive }">Frank</template>
+<template #tab0="{ value, isActive }">
+<p>到<code v-pre>[1003 139 -4853]</code>收集一个<code v-pre>Snail Slime</code></p>
+<p>到地方了之后是一个屋子，进去屋子里面会有个洞，往下走就到了</p>
+<p>走到最下面会有一个NPC，进去同意竞赛，开始后直接窜到终点线</p>
+<p>挂机等对手到就行了</p>
+</template>
+<template #tab1="{ value, isActive }">
+<p></p>
+</template>
+<template #tab2="{ value, isActive }">
+<p></p>
+</template>
+</Tabs>
+<p>7.前往收集第二个材料</p>
+<Tabs id="63" :data='[{"id":"Brie"},{"id":"Kale"},{"id":"Frank"}]' tab-id="chef">
+<template #title0="{ value, isActive }">Brie</template>
+<template #title1="{ value, isActive }">Kale</template>
+<template #title2="{ value, isActive }">Frank</template>
+<template #tab0="{ value, isActive }">
+<p>到<code v-pre>[1215 126 -4932]</code>收集一个<code v-pre>Moonsalt</code></p>
+<p>进去塔楼，塔楼内的白色装置站上去后会颠倒重力</p>
+<p>反正慢慢找路，找没去过的路就行了，很好找！</p>
+<p>最后上到塔顶再踩一下反转装置，来到月球</p>
+<p>跟旁边NPC小人对话一下(只有一个)，然后来到<code v-pre>[2488 36 -5252]</code>的洞穴</p>
+<p>进去拿到盐之后跑路，把盐塞给<code v-pre>[2506 22 -5168]</code>处的装置</p>
+<p>然后去火箭，开润</p>
+</template>
+<template #tab1="{ value, isActive }">
+<p></p>
+</template>
+<template #tab2="{ value, isActive }">
+<p></p>
+</template>
+</Tabs>
+<p>8.前往收集第三个材料</p>
+<Tabs id="101" :data='[{"id":"Brie"},{"id":"Kale"},{"id":"Frank"}]' tab-id="chef">
+<template #title0="{ value, isActive }">Brie</template>
+<template #title1="{ value, isActive }">Kale</template>
+<template #title2="{ value, isActive }">Frank</template>
+<template #tab0="{ value, isActive }">
+<p>坐<code v-pre>[974 42 -4441]</code>的升降机下虚空</p>
+<p>然后摸黑走到<code v-pre>[2078 64 -4610]</code>拿到虚空根</p>
+<p>怎么走？下来出生点后绕一圈看到Ahmsord掉下来的货物，然后直接往左走，第一条路就是</p>
+</template>
+<template #tab1="{ value, isActive }">
+<p></p>
+</template>
+<template #tab2="{ value, isActive }">
+<p></p>
+</template>
+</Tabs>
+<p>9.全部材料收集完后，回去把材料带给你的厨师</p>
+<p>10.和Chef Hamsey对话，进他的私人厨房</p>
 <p>7把食物给Chef Hamsey吃</p>
-<p 胸甲="">奖励<br>
-4200000经验<br>
-4096绿宝石<br>
-Chef Hamsey's Apron (Brie)<br>
-Smoldering Apron (Kale)<br>
-Sky Chef's Apron (Frank)</p>
+<p>奖励</p>
+<ul>
+<li>4200000经验</li>
+<li>4096绿宝石</li>
+<li>Chef Hamsey's Apron (Brie)</li>
+<li>Smoldering Apron (Kale)</li>
+<li>Sky Chef's Apron (Frank)</li>
+<li>可以使用Juggler的快速旅行了(Cinfras→Ahmsord 单向传送) (橙色柱子)</li>
+</ul>
 </div></template>
 
 
--- src/quests/lvl91-100/level 96 - Recipe for Disaster.md
+++ src/quests/lvl91-100/level 96 - Recipe for Disaster.md
@@ -3,24 +3,85 @@
 :::warning 
 此页面有待更新！
 :::
-1跟在Ahmsord的Chef Hamsey对话X 949 Y 129 Z -4475
+1跟在**Ahmsord**的*Chef Hamsey*对话`[949 129 -4475]`
 
-2进去Chef Hamsey的厨房
+2进去*Chef Hamsey*的厨房(身后)，在里面和跟*Chef Hamsey*谈话对话
 
 3跟Chef Hamsey谈话 然后跟其他三个npc谈话
 
-4选择一个厨师继续任务
+4再次跟Chef Hamsey谈话，选择一个厨师继续任务，然后找对应的NPC接活
+:::tip
+NPC会给你一本食谱书，打开即可选择追踪材料
 
-5到NPC给你的坐标的位置收集物品(背包里有本书记录了任务坐标,基本都是掉落物)
+一共需要收集三个材料
+:::
 
-6把材料给Chef Hamsey
+6.前往收集第一个材料
+::: tabs#chef
+
+@tab Brie#Brie
+到`[1003 139 -4853]`收集一个`Snail Slime`
+
+到地方了之后是一个屋子，进去屋子里面会有个洞，往下走就到了
+
+走到最下面会有一个NPC，进去同意竞赛，开始后直接窜到终点线
+
+挂机等对手到就行了
+@tab Kale#Kale
+
+@tab Frank#Frank
+
+:::
+
+7.前往收集第二个材料
+::: tabs#chef
+
+@tab Brie#Brie
+到`[1215 126 -4932]`收集一个`Moonsalt`
+
+进去塔楼，塔楼内的白色装置站上去后会颠倒重力
+
+反正慢慢找路，找没去过的路就行了，很好找！
+
+最后上到塔顶再踩一下反转装置，来到月球
+
+跟旁边NPC小人对话一下(只有一个)，然后来到`[2488 36 -5252]`的洞穴
+
+进去拿到盐之后跑路，把盐塞给`[2506 22 -5168]`处的装置
+
+然后去火箭，开润
+@tab Kale#Kale
+
+@tab Frank#Frank
+
+:::
+
+8.前往收集第三个材料
+::: tabs#chef
+
+@tab Brie#Brie
+坐`[974 42 -4441]`的升降机下虚空
+
+然后摸黑走到`[2078 64 -4610]`拿到虚空根
+
+怎么走？下来出生点后绕一圈看到Ahmsord掉下来的货物，然后直接往左走，第一条路就是
+@tab Kale#Kale
+
+@tab Frank#Frank
+
+:::
+
+
+9.全部材料收集完后，回去把材料带给你的厨师
+
+10.和Chef Hamsey对话，进他的私人厨房
 
 7把食物给Chef Hamsey吃
 
 奖励
-4200000经验
-4096绿宝石
-Chef Hamsey's Apron (Brie) 
-Smoldering Apron (Kale) 
-Sky Chef's Apron (Frank)
-{胸甲}
++ 4200000经验
++ 4096绿宝石
++ Chef Hamsey's Apron (Brie) 
++ Smoldering Apron (Kale) 
++ Sky Chef's Apron (Frank)
++ 可以使用Juggler的快速旅行了(Cinfras→Ahmsord 单向传送) (橙色柱子)
