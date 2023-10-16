94facee691a8d1907d3cbd4b95ed591cd8b9de20
EternityTQ <1586049354@qq.com>
2023-10-16 16:25:10 +0800

    lvl 2

--- src/.vuepress/.temp/pages/quests/lvl1-10/level 2 - Poisoning the Pest.html.vue
+++ src/.vuepress/.temp/pages/quests/lvl1-10/level 2 - Poisoning the Pest.html.vue
@@ -2,7 +2,7 @@
 <p><strong>铲除虫害</strong></p>
 <p>1.和Ragni城旁边的<em>Farmer Cevalus</em><code v-pre>[-748 64 -1663]</code>说话</p>
 <p>2.在任务NPC旁边看见一个老鼠药，攻击掉落</p>
-<p>3.进入<em>Farmer Kelri</em>旁边的农田，将老鼠药右键里面的水。</p>
+<p>3.进入<em>Farmer Kelri</em>旁边的农田，手持老鼠药右键里面的水。</p>
 <p>4.投毒后进入战斗，杀死腐化的农夫，随后出门与Farmer Cevalus对话完成任务</p>
 <p>奖励</p>
 <ul>
