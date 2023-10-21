11d1b0598b1a0a097f1ca394b390483be0055dfc
EternityTQ <1586049354@qq.com>
2023-10-22 02:33:05 +0800

    font update

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -10,7 +10,23 @@ export default defineUserConfig({
   description: "Wynncraft中文攻略",
 
   theme,
+  head: [
+    // ...
 
+    // 导入相应链接
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swa",
+        rel: "stylesheet",
+      },
+    ],
+  ],
   plugins: [
     
     searchProPlugin({
