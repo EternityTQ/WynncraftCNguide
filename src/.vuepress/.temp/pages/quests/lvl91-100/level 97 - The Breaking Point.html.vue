660d860722112bb1a6f3af91c461f8a51b1fbc2e
EternityTQ <1586049354@qq.com>
2023-10-27 11:52:09 +0800

    The Breaking Point update

--- src/.vuepress/.temp/pages/quests/lvl91-100/level 97 - The Breaking Point.html.vue
+++ src/.vuepress/.temp/pages/quests/lvl91-100/level 97 - The Breaking Point.html.vue
@@ -10,10 +10,6 @@
 <li><RouterLink to="/quests/lvl91-100/level%2094%20-%20Dwarves%20and%20Doguns%20Part%20IV.html">Dwarves and Doguns Part IV (level 94)</RouterLink></li>
 <li><RouterLink to="/quests/lvl81-90/level%2089%20-%20The%20Envoy%20Part%20II.html">The Envoy Part II (Level 89)</RouterLink></li>
 </ul>
-<div class="hint-container warning">
-<p class="hint-container-title">注意</p>
-<p>此页面有待更新！</p>
-</div>
 <p>1.和<strong>Kandon-Beda</strong>的<em>General Elix</em><code v-pre>[756 164 -4463]</code>对话</p>
 <p>2.和<em>Rhay</em><code v-pre>[770 162 -4385]</code>对话，搭乘热气球前往Corkus，路上你可以向Rhay提问一些和地震有关的问题</p>
 <p>3.来到Corkus岛，触发剧情，然后径直进入房间内的洞穴内</p>
@@ -28,6 +24,35 @@
 <p>6.离开洞穴，登上热气球，回到<strong>Kandon-Beda</strong>，跟着<em>Rhay</em>触发剧情</p>
 <p>7.回到刚才的热气球，触发剧情</p>
 <p>8.来到<strong>Molten Heights</strong><code v-pre>[1466 141 -5316]</code>后，跟着<em>Korzim</em>进入传送门内</p>
+<p>9.剧情结束后，前往<strong>Lower Freezing Heights</strong><code v-pre>[1512 34 -5468]</code>，和<em>King Draani</em>对话</p>
+<p>10.进入洞穴内，剧情结束后使用抓钩飞跃峡谷，按下尽头的按钮</p>
+<div class="hint-container tip">
+<p class="hint-container-title">提示</p>
+<p>洞穴内全程禁止释放位移技能</p>
+<p>抓钩只能对准<font color="blue"><strong>蓝色岩石</strong></font>右键进行抓取</p>
+<p>类似于弓箭手的抓钩，但是其非常受延迟影响，需要注意预判</p>
+<p>你可以站在黄色光圈的提示点对准蓝色岩石进行抓取</p>
+<p>需要注意的是，有类似于从底下跨越岩石的情况，即掉下去后瞬间拉取蓝色岩石</p>
+<p>第一个存档点后会有两条分歧点，走上边快一点但难度略高，走下面绕一段路但是能省去一个较难点</p>
+</div>
+<p>11.进入被封印的大门内，继续对话，选项任意选</p>
+<p>12.离开洞穴，返回<strong>Rodoroc</strong>城外<code v-pre>[1233 9 -5200]</code></p>
+<p>13.通过<strong>Rodoroc</strong>城内的捷径<code v-pre>[1015 12 -5062]</code>前往<strong>Thanos</strong>，随后离开<strong>Thanos</strong>，出门右转，前往<strong>Thanos</strong>顶层的龙巢<code v-pre>[206 97 -5179]</code></p>
+<p>14.进入巨龙的嘴内，取出<code v-pre>Everflame</code></p>
+<p>15.出来后，通过传送门返回<strong>Rodoroc</strong>，对话完后继续回到第9步的洞穴<code v-pre>[1512 34 -5468]</code></p>
+<p>16.用<code v-pre>Everflame</code>融化每一个房间，随后继续剧情</p>
+<p>奖励</p>
+<ul>
+<li>4500000 经验值</li>
+<li>12288 绿宝石</li>
+<li>4 Tol Runes</li>
+<li>1 Ancient Dogun Crystal</li>
+<li>可以进入RAID - The Canyon Colossus了</li>
+</ul>
+<div class="hint-container tip">
+<p class="hint-container-title">提示</p>
+<p>只要有任意角色完成该任务，其他角色就可以直接解锁tcc</p>
+</div>
 </div></template>
 
 
--- src/quests/lvl91-100/level 97 - The Breaking Point.md
+++ src/quests/lvl91-100/level 97 - The Breaking Point.md
@@ -14,9 +14,6 @@ icon: skull
 + [Dwarves and Doguns Part IV (level 94)](/WynncraftCNguide/quests/lvl91-100/level%2094%20-%20Dwarves%20and%20Doguns%20Part%20IV.html)
 + [The Envoy Part II (Level 89)](/WynncraftCNguide/quests/lvl81-90/level%2089%20-%20The%20Envoy%20Part%20II.html)
 
-:::warning 
-此页面有待更新！
-:::
 
 1.和**Kandon-Beda**的*General Elix*`[756 164 -4463]`对话
 
@@ -36,3 +33,44 @@ icon: skull
 7.回到刚才的热气球，触发剧情
 
 8.来到**Molten Heights**`[1466 141 -5316]`后，跟着*Korzim*进入传送门内
+
+9.剧情结束后，前往**Lower Freezing Heights**`[1512 34 -5468]`，和*King Draani*对话
+
+10.进入洞穴内，剧情结束后使用抓钩飞跃峡谷，按下尽头的按钮
+
+:::tip
+洞穴内全程禁止释放位移技能
+
+抓钩只能对准<font color="blue">**蓝色岩石**</font>右键进行抓取
+
+类似于弓箭手的抓钩，但是其非常受延迟影响，需要注意预判
+
+你可以站在黄色光圈的提示点对准蓝色岩石进行抓取
+
+需要注意的是，有类似于从底下跨越岩石的情况，即掉下去后瞬间拉取蓝色岩石
+
+第一个存档点后会有两条分歧点，走上边快一点但难度略高，走下面绕一段路但是能省去一个较难点
+:::
+
+11.进入被封印的大门内，继续对话，选项任意选
+
+12.离开洞穴，返回**Rodoroc**城外`[1233 9 -5200]`
+
+13.通过**Rodoroc**城内的捷径`[1015 12 -5062]`前往**Thanos**，随后离开**Thanos**，出门右转，前往**Thanos**顶层的龙巢`[206 97 -5179]`
+
+14.进入巨龙的嘴内，取出`Everflame`
+
+15.出来后，通过传送门返回**Rodoroc**，对话完后继续回到第9步的洞穴`[1512 34 -5468]`
+
+16.用`Everflame`融化每一个房间，随后继续剧情
+
+奖励
++ 4500000 经验值
++ 12288 绿宝石
++ 4 Tol Runes
++ 1 Ancient Dogun Crystal
++ 可以进入RAID - The Canyon Colossus了
+
+:::tip
+只要有任意角色完成该任务，其他角色就可以直接解锁tcc
+:::
