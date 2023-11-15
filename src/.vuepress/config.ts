734cf9b079a1662c0ed00ea46b32a8ef78477153
EternityTQ <1586049354@qq.com>
2023-11-16 01:45:49 +0800

    bug fixed

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,11 +9,6 @@ export default defineUserConfig({
   title: "Wynncraft中文攻略",
   description: "Wynncraft中文攻略",
 
-  extendsMarkdown: (md) => {
-    md.use(plugin1)
-    md.linkify.set({ fuzzyEmail: false })
-  },
-  
   theme,
   head: [
     // ...
