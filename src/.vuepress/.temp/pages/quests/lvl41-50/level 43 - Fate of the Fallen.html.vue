744459920a5c2d1b6c11ffbb7a4f1e2390285064
EternityTQ <1586049354@qq.com>
2023-12-10 17:27:17 +0800

    bug fixed

--- src/.vuepress/.temp/pages/quests/lvl41-50/level 43 - Fate of the Fallen.html.vue
+++ src/.vuepress/.temp/pages/quests/lvl41-50/level 43 - Fate of the Fallen.html.vue
@@ -4,24 +4,24 @@
 <p class="hint-container-title">重要任务</p>
 <p>解锁Ice Barrows副本</p>
 </div>
-<p>1.和<em>Telvu</em><code v-pre>[165, 74, -675]</code>对话</p>
-<p>2.在<strong>Nesaak</strong>的银行内拿到<em>Theorick</em>的权杖</p>
+<p>1.和<font color=00AA00><strong>Telvu</strong></font><code v-pre>[165, 74, -675]</code>对话</p>
+<p>2.在<strong>Nesaak</strong>的银行内拿到<font color=00AA00><strong>Theorick</strong></font>的权杖，带回去交给<font color=00AA00><strong>Telvu</strong></font><code v-pre>[165, 74, -675]</code></p>
 <blockquote>
 <p>在旁边找条路上去即可 很好找</p>
 </blockquote>
-<figure><img src="@source/.vuepress/public/assets/img/lvl43-1.jpg" alt="" tabindex="0" loading="lazy"><figcaption></figcaption></figure>
-<p>3.进入法师的传送门</p>
+<figure><img src="/assets/img/lvl43-1.jpg" alt="" tabindex="0" loading="lazy"><figcaption></figcaption></figure>
+<p>3.进入<font color=00AA00><strong>Telvu</strong></font>开启的传送门</p>
 <p>4.进入时间之门<code v-pre>[-582 70 -1176]</code>，利用权杖回到过去</p>
 <p>5.探索过去的<strong>Nesaak</strong>，和NPC<code v-pre>[3796 73 1185]</code>对话</p>
 <p>6.来到<code v-pre>[3651 69 1208]</code>触发剧情</p>
-<p>7.在<em>Theorick</em>的基地和众人回合<code v-pre>[3738 67 1265]</code></p>
-<p>8.在<em>Theorick</em>进入传送门后，自己被弹走至断桥前，保护传送门，击杀怪物，将指定掉落物丢进漏斗内</p>
+<p>7.在<font color=00AA00><strong>Theorick</strong></font>的基地和众人回合<code v-pre>[3738 67 1265]</code></p>
+<p>8.在<font color=00AA00><strong>Theorick</strong></font>进入传送门后，自己被弹走至断桥前，保护传送门，击杀怪物，将指定掉落物丢进漏斗内</p>
 <div class="hint-container tip">
 <p class="hint-container-title">提示</p>
-<p>在<em>Theorick</em>的进去前的对话结束后，如果你能在被弹走前使用位移技能快速冲到传送门前，则可以直接跳过杀怪的流程</p>
+<p>在<font color=00AA00><strong>Theorick</strong></font>的进去前的对话结束后，如果你能在被弹走前使用位移技能快速冲到传送门前，则可以直接跳过杀怪的流程</p>
 </div>
 <p>9.桥建好后，靠近传送门触发剧情</p>
-<p>10.告诉<em>Telvu</em>过去发生了什么</p>
+<p>10.告诉<font color=00AA00><strong>Telvu</strong></font>过去发生了什么</p>
 <p>奖励</p>
 <ul>
 <li>20000 经验值</li>
--- src/quests/lvl41-50/level 43 - Fate of the Fallen.md
+++ src/quests/lvl41-50/level 43 - Fate of the Fallen.md
@@ -9,14 +9,14 @@ icon: skull
 解锁Ice Barrows副本
 :::
 
-1.和*Telvu*`[165, 74, -675]`对话
+1.和<font color=00AA00>**Telvu**</font>`[165, 74, -675]`对话
 
-2.在**Nesaak**的银行内拿到*Theorick*的权杖
+2.在**Nesaak**的银行内拿到<font color=00AA00>**Theorick**</font>的权杖，带回去交给<font color=00AA00>**Telvu**</font>`[165, 74, -675]`
 >在旁边找条路上去即可 很好找
 
-![](../../.vuepress/public/assets/img/lvl43-1.jpg)
+![](/assets/img/lvl43-1.jpg)
 
-3.进入法师的传送门
+3.进入<font color=00AA00>**Telvu**</font>开启的传送门
 
 4.进入时间之门`[-582 70 -1176]`，利用权杖回到过去
 
@@ -24,17 +24,17 @@ icon: skull
 
 6.来到`[3651 69 1208]`触发剧情
 
-7.在*Theorick*的基地和众人回合`[3738 67 1265]`
+7.在<font color=00AA00>**Theorick**</font>的基地和众人回合`[3738 67 1265]`
 
-8.在*Theorick*进入传送门后，自己被弹走至断桥前，保护传送门，击杀怪物，将指定掉落物丢进漏斗内
+8.在<font color=00AA00>**Theorick**</font>进入传送门后，自己被弹走至断桥前，保护传送门，击杀怪物，将指定掉落物丢进漏斗内
 
 :::tip
-在*Theorick*的进去前的对话结束后，如果你能在被弹走前使用位移技能快速冲到传送门前，则可以直接跳过杀怪的流程
+在<font color=00AA00>**Theorick**</font>的进去前的对话结束后，如果你能在被弹走前使用位移技能快速冲到传送门前，则可以直接跳过杀怪的流程
 :::
 
 9.桥建好后，靠近传送门触发剧情
 
-10.告诉*Telvu*过去发生了什么
+10.告诉<font color=00AA00>**Telvu**</font>过去发生了什么
 
 奖励
 
