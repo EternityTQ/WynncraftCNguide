9f2c690b57a154d696047767a361e3e6d5df3059
EternityTQ <1586049354@qq.com>
2024-02-01 15:33:43 +0800

    删除无效内容

--- src/.vuepress/.temp/pages/newbie/lvl31-40/discovery.html.js
+++ src/.vuepress/.temp/pages/newbie/lvl31-40/discovery.html.js
@@ -1,4 +1,4 @@
-export const data = JSON.parse("{\"key\":\"v-71141017\",\"path\":\"/newbie/lvl31-40/discovery.html\",\"title\":\"探索区域\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"探索区域\",\"icon\":\"map\",\"description\":\":::danger 这个页面被暂时用作测试页面了 请知悉:( ::: 1111 箭弹 使用连招： 左键 - 右键 - 右键 另一种能力 另一种连招： 按键1 - 按键2 - 按键3 export default { data() { return { isTooltipVisible: false, currentTooltip: '', toolti...\"},\"headers\":[],\"readingTime\":{\"minutes\":0.82,\"words\":245},\"filePathRelative\":\"newbie/lvl31-40/discovery.md\",\"autoDesc\":true}")
+export const data = JSON.parse("{\"key\":\"v-71141017\",\"path\":\"/newbie/lvl31-40/discovery.html\",\"title\":\"探索区域\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"探索区域\",\"icon\":\"map\",\"description\":\"\"},\"headers\":[],\"readingTime\":{\"minutes\":0.02,\"words\":7},\"filePathRelative\":\"newbie/lvl31-40/discovery.md\",\"autoDesc\":true}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
