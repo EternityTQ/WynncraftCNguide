8553c358d0595197c780a035a739efb8a7a97d37
EternityTQ <1586049354@qq.com>
2024-03-24 22:28:18 +0800

    1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { defineClientConfig } from "vuepress/client";
 import { defineSearchConfig } from "vuepress-plugin-search-pro/client";
+import { oml2dPlugin } from 'vuepress-plugin-oh-my-live2d';
 
 export default defineUserConfig({
   base: "/",
@@ -31,7 +32,30 @@ export default defineUserConfig({
     
     ['link', { rel: 'Minecraft', href: '/assets/font/Minecraft.ttf' }],
   ],
-  
+  plugins: [
+    oml2dPlugin({
+      // 在这里配置选项
+      menus: {
+        // 在这里配置
+        disable:true,
+      },
+      models: [
+        {
+          path: 'https://raw.githubusercontent.com/EternityTQ/WynncraftCNguide/main/src/.vuepress/public/09kohane_unit/09kohane_unit.model3.json',
+          scale: 0.12,
+          position: [-10, 90],
+          volume: 0.1,
+          showHitAreaFrames:true,
+          stageStyle: {
+            width: 350
+          },
+          
+        }
+      ]
+    })
+
+    //  ...other plugins
+  ],
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
