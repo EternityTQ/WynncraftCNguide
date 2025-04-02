27a3ff11f250779e231354400960f77c962c4257
EternityTQ <1586049354@qq.com>
2025-04-02 22:09:29 +0800

    搜索插件

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -126,6 +126,7 @@ export default hopeTheme({
   // 在这里配置主题提供的插件
   plugins: {
     photoSwipe:false,
+    slimsearch:true,
     // 注意: 仅用于测试! 你必须自行生成并在生产环境中使用自己的评论服务
     comment: {
       provider: "Waline",
@@ -136,7 +137,7 @@ export default hopeTheme({
     },
 
     components: {
-      components: ["Badge", "VPCard","CopyCoord"],
+      components: ["Badge", "VPCard"],
     },
 
     
